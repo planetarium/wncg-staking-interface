d8642a50fc454d5f11bbd1052635ee1cff07b570
Kimbug <kimbug@planetariumhq.com>
2022-07-26 16:57:09 +0900

    🚧 Create a redux pool state

---
+++ app/states/pool.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from 'app/store'
+
+type PoolState = {
+  pool: Pool | null
+}
+
+const INITIAL_STATE: PoolState = {
+  pool: null,
+}
+
+const poolSlice = createSlice({
+  name: '#pool',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setPool(state: PoolState, action: PayloadAction<Pool>) {
+      state.pool = action.payload
+    },
+  },
+})
+
+export const { setPool } = poolSlice.actions
+export default poolSlice.reducer
+
+// Selector
+export function getPool(state: RootState): Pool | null {
+  return state.pool.pool
+}
