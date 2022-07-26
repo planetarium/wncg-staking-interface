4e2f17575b3e953db77811927258a16e2c5aa3cb
Kimbug <kimbug@planetariumhq.com>
2022-07-26 16:56:05 +0900

    🚧 Create a pool page

---
+++ pages/wncg/pool.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const WncgPool: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>WNCG-WETH Pool / WNCG Staking</title>
+      </Head>
+
+      <main>main</main>
+    </>
+  )
+}
+
+export default WncgPool
