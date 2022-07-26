368485e4557ba82083f6ddc814105448d61abf72
Kimbug <kimbug@planetariumhq.com>
2022-07-26 16:51:37 +0900

    ♻️ Create CoingeckoService to fetch prices

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-export const coingecko = axios.create({
+const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_COINGECKO_API_BASE_URL as string,
 })
+
+export default instance
