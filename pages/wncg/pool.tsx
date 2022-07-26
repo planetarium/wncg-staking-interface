517753d24120005dfdcae8cdc4c24a48b3927f7e
Kimbug <kimbug@planetariumhq.com>
2022-07-26 17:28:13 +0900

    💄 Add bg in pool page

--- pages/wncg/pool.tsx
+++ pages/wncg/pool.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import clsx from 'clsx'
+import styles from 'styles/Pool.module.scss'
 
+import { getIsDesktop } from 'app/states/mediaQuery'
 import { getPool } from 'app/states/pool'
 import { useAppSelector, usePool } from 'hooks'
 
 const WncgPool: NextPage = () => {
   const pool = useAppSelector(getPool)
   usePool()
+
+  const isDesktop = useAppSelector(getIsDesktop)
 
   return (
     <>
@@ -16,6 +22,17 @@ const WncgPool: NextPage = () => {
       {JSON.stringify(pool)}
 
       <main>main</main>
+
+      {isDesktop && (
+        <div className={styles.bgWrapper} aria-hidden>
+          <div className={clsx(styles.bg, styles.cash)}>
+            <Image src="/img-bg-cash.png" layout="fill" priority alt="" />
+          </div>
+          <div className={clsx(styles.bg, styles.human)}>
+            <Image src="/img-bg-human.png" layout="fill" priority alt="" />
+          </div>
+        </div>
+      )}
     </>
   )
 }
