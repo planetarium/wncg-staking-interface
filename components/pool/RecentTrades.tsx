83dc97e091ad9a2dd3fd71749187977fd3930ae4
Kimbug <kimbug@planetariumhq.com>
2022-07-27 10:17:56 +0900

    🚧 Fetch recent swaps

--- components/pool/RecentTrades.tsx
+++ components/pool/RecentTrades.tsx
@@ -1,10 +1,32 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
+import { useQuery } from 'react-query'
 import styles from './styles/RecentTrades.module.scss'
 
+import { fetchPoolRecentSwaps } from 'lib/graphql'
+
 function PoolRecentTrades() {
+  const [skip, setSkip] = useState(0)
+  const [swaps, setSwaps] = useState<Swap[]>([])
+
+  useQuery(['recentSwaps', skip], () => fetchPoolRecentSwaps(skip * 5, 5), {
+    staleTime: 10 * 1_000,
+    keepPreviousData: true,
+    onSuccess: (data) => {
+      setSwaps((prev) => [...prev, ...data])
+    },
+  })
+
+  function loadMore() {
+    setSkip((prev) => prev + 1)
+  }
+
   return (
     <section className={styles.poolRecentTrades}>
       <h3>Recent Trades</h3>
+      <div>{JSON.stringify(swaps)}</div>
+      <button type="button" onClick={loadMore}>
+        Load More
+      </button>
     </section>
   )
 }
---
+++ types/swap.d.ts
@@ -0,0 +1,11 @@
+type Swap = {
+  tokenIn: string
+  tokenOut: string
+  tokenAmountIn: string
+  tokenAmountOut: string
+  valueUSD: string
+  userAddress: {
+    id: string
+  }
+  timestamp: number
+}
