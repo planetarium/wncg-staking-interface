9becb34a1d3587718670633e4345a5a7606c683b
Kimbug <kimbug@planetariumhq.com>
2022-07-29 14:56:27 +0900

    🚧 Displace common components in withdraw page

--- components/pool/WithdrawForm/index.tsx
+++ components/pool/WithdrawForm/index.tsx
@@ -1,12 +1,12 @@
 import { memo } from 'react'
 import styles from '../styles/Form.module.scss'
 
-function PoolInvestForm() {
+function PoolWithdrawForm() {
   return (
     <section className={styles.formSection}>
-      <h3 className={styles.title}>Invest in pool</h3>
+      <h3 className={styles.title}>Withdraw from pool</h3>
     </section>
   )
 }
 
-export default memo(PoolInvestForm)
+export default memo(PoolWithdrawForm)
--- pages/wncg/pool/withdraw.tsx
+++ pages/wncg/pool/withdraw.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import styles from 'styles/Pool.module.scss'
 
 import PageWrapper from 'components/StakingPageWrapper'
+import MyWallet from 'components/pool/MyWallet'
+import MyBalance from 'components/pool/MyBalance'
+import WithdrawForm from 'components/pool/WithdrawForm'
 
 const WncgPoolWithdraw: NextPage = () => {
   return (
@@ -12,7 +15,19 @@ const WncgPoolWithdraw: NextPage = () => {
       </Head>
 
       <PageWrapper>
-        <div className={styles.container}>Withdraw</div>
+        <div className={styles.container}>
+          <h1 className="hidden">Invest</h1>
+
+          <div className={styles.left}>
+            <MyWallet />
+          </div>
+          <div className={styles.center}>
+            <WithdrawForm />
+          </div>
+          <div className={styles.right}>
+            <MyBalance />
+          </div>
+        </div>
       </PageWrapper>
     </>
   )
