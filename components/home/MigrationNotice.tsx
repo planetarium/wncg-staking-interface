91599d6a48583d7996a14ed7765b37ac73dac6e5
Kimbug <kimbug@planetariumhq.com>
2022-09-08 19:24:42 +0900

    💄 Create & Style MigrationNotice

---
+++ components/home/MigrationNotice.tsx
@@ -0,0 +1,59 @@
+import { useMemo } from 'react'
+import { useRecoilValue } from 'recoil'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+import styles from './styles/MigrationNotice.module.scss'
+
+import { isMobileState } from 'app/states/mediaQuery'
+import {
+  migrationGuideTransitionDesktop,
+  migrationGuideTransitionMobile,
+  migrationGuideVariants,
+} from './constants'
+
+import { Icon } from 'components/Icon'
+
+export function MigrationNotice() {
+  const isMobile = useRecoilValue(isMobileState)
+
+  const transition = useMemo(
+    () =>
+      isMobile
+        ? migrationGuideTransitionMobile
+        : migrationGuideTransitionDesktop,
+    [isMobile]
+  )
+
+  return (
+    <motion.aside
+      className={styles.migrationNotice}
+      key="migrationNotice"
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={transition}
+      variants={migrationGuideVariants}
+    >
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          Please migrate to the new WNCG Staking contract!
+          <Icon id="chevronRight" ariaHidden />
+        </h1>
+        <p className={styles.desc}>
+          Current contract will no longer distribute the rewards from early
+          October.
+        </p>
+      </div>
+
+      <div className={styles.figure} aria-hidden>
+        <Image
+          src="/img-migration.png"
+          layout="fill"
+          objectFit="contain"
+          priority
+          alt=""
+        />
+      </div>
+    </motion.aside>
+  )
+}
--- pages/wncg/index.tsx
+++ pages/wncg/index.tsx
@@ -1,16 +1,21 @@
+import { useRecoilValue } from 'recoil'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { NextSeo } from 'next-seo'
+import { AnimatePresence } from 'framer-motion'
 import styles from 'styles/Wncg.module.scss'
 
+import { legacyModeState } from 'app/states/settings'
 import { STAKING_SEO } from 'lib/seo'
 
 import PageWrapper from 'components/StakingPageWrapper'
 import { Content } from 'components/home/Content'
 import { Dashboard } from 'components/home/Dashboard'
 import { HomeHeader } from 'components/home/Header'
+import { MigrationNotice } from 'components/home/MigrationNotice'
 
 const WncgStaking: NextPage = () => {
+  const legacyMode = useRecoilValue(legacyModeState)
   return (
     <>
       <NextSeo {...STAKING_SEO} />
@@ -25,6 +30,7 @@ const WncgStaking: NextPage = () => {
       <PageWrapper showBg>
         <div className={styles.container}>
           <HomeHeader />
+          <AnimatePresence>{legacyMode && <MigrationNotice />}</AnimatePresence>
           <Dashboard />
           <Content />
         </div>
