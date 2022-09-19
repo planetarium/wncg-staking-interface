21e26b30ff43af4605304ec116862d4f65added8
Kimbug <kimbug@planetariumhq.com>
2022-09-19 14:16:22 +0900

    🚧 Update useQuery option

--- hooks/usePrices.ts
+++ hooks/usePrices.ts
@@ -9,9 +9,8 @@ import { fetchNativeAssetPrice, fetchTokenPrices } from 'services/price'
 import { usePool } from './usePool'
 
 const options = {
-  retry: 2,
-  staleTime: (60 - 10) * 1_000,
-  refetchInterval: 60 * 1_000,
+  retry: false,
+  staleTime: 60 * 1_000,
   keepPreviousData: true,
 }
 
