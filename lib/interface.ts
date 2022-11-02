cc3e925b042de4ee667f8d9678889ee24b0f664f
Kimbug <kimbug@planetariumhq.com>
2022-11-02 10:55:23 +0900

    🚧 Add WETH abi

--- lib/interface.ts
+++ lib/interface.ts
@@ -4,9 +4,11 @@ import {
   Erc20Abi,
   LiquidityGaugeAbi,
   StakingAbi,
+  WETHAbi,
 } from './abi'
 
 export const ercTokenIface = new Interface(Erc20Abi)
 export const liquidityGaugeIface = new Interface(LiquidityGaugeAbi)
 export const stakingIface = new Interface(StakingAbi)
 export const vaultIface = new Interface(BalancerVaultAbi)
+export const wethIface = new Interface(WETHAbi)
