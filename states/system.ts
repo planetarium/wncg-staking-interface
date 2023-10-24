52a9ae21196e71bf55abca1ad6291e6f08fbe2f7
Kimbug <kimbug@planetariumhq.com>
2023-10-24 15:56:21 +0900

    refactor(atom): delete unused jotai states

--- states/system.ts
+++ states/system.ts
@@ -2,13 +2,6 @@ import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 export const currentTimestampAtom = atom(0)
-export const totalStakedAtom = atom<string | null>(null)
-
-export type AssetPlatform = 'ethereum' | 'bsc'
-
-export const assetPlatformAtom = atom<AssetPlatform>('ethereum')
-
-export const currentChainAtom = atom<Chain | null>(null)
 
 export const slippageAtom = atomWithStorage<string | null>(
   `wncg:staking:slippage`,
