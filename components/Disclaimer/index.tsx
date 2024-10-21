69af03b0fca681c9dbfae8c3c8cab332eccd305f
jaycho46 <jay@planetariumhq.com>
2024-10-21 18:05:21 +0900

    fix: no-unescaped-entities on single quote

--- components/Disclaimer/index.tsx
+++ components/Disclaimer/index.tsx
@@ -4,7 +4,7 @@ import { StyledDisclaimer } from './styled'
 function Disclaimer() {
   return (
     <StyledDisclaimer>
-      By connecting a wallet, you agree to Nine Chronicles Ltd's{' '}
+      By connecting a wallet, you agree to Nine Chronicles Ltd&apos;s{' '}
       <Link href="/docs/terms" target="_blank" rel="noopener">
         Terms of Service
       </Link>{' '}
